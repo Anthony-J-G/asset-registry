de980f4c7e41c7a640be908d2141488ac29bcf1f
anthony-j-g <anthony-j-g@frostmarch>
2025-07-26 11:22:46 -0700

    [libogg] updating portfile SHA256

--- ports/libogg/portfile.cmake
+++ ports/libogg/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO xiph/ogg
     REF v1.3.3
-    SHA512 0bd6095d647530d4cb1f509eb5e99965a25cc3dd9b8125b93abd6b248255c890cf20710154bdec40568478eb5c4cde724abfb2eff1f3a04e63acef0fbbc9799b
+    SHA256 b69687fd9172a107b7d3e2e595c806509ec6a99e
     HEAD_REF master
 )
 
