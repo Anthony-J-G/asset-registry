01b4eacefabf30b060fd420cc8250538f9776c84
anthony-j-g <anthony-j-g@frostmarch>
2025-07-26 11:35:40 -0700

    [libogg] updating port

--- ports/libogg/portfile.cmake
+++ ports/libogg/portfile.cmake
@@ -2,10 +2,9 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO xiph/ogg
     REF v1.3.3
-    SHA256 b69687fd9172a107b7d3e2e595c806509ec6a99e
+    SHA512 0
     HEAD_REF master
 )
-
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_cmake_install()
